9da6ed5caa418ba7b3e7fb2ad95aff2dec519c4b
KrammyBoy <salabsabmarkdanielle@gmail.com>
2025-08-28 23:52:56 +0800

    Checking

--- src/app/Models/DBContext.php
+++ src/app/Models/DBContext.php
@@ -52,10 +52,7 @@ class DBContext {
                     $dbName = getenv('DB_NAME')     ?: 'postgres';
                     $dbUser = getenv('DB_USER')     ?: 'postgres';
                     $dbPass = getenv('DB_PASSWORD') ?: '';
-                }
-                var_dump(getenv('RAILWAY_SERVICE_NAME'));
-                    var_dump(getenv('DATABASE_URL'));
-                    exit();                
+                }          
                 //Create the connection string
                 $dsn = "pgsql:host=$host;port=$port;dbname=$dbName";
 
